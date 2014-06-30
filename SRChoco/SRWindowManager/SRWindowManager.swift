4816a3186024fd19e27136647128a8894b1aa58a
seorenn <hirenn@iplateia.com>
2014-06-30 16:12:21 +0900

    Added OS determiner -_-

--- SRChoco/SRWindowManager/SRWindowManager.swift
+++ SRChoco/SRWindowManager/SRWindowManager.swift
@@ -1,20 +1,24 @@
 #if os(OSX)
-    
 import Cocoa
+#endif
     
 class SRWindowManager {
     // NOTE: Crashes on Xcode 6 beta 2
     func processes() -> NSRunningApplication[]! {
+        #if os(iOS)
+            assert(false, "iOS(UIKit) does not support this feature")
+        #endif
         let apps = NSWorkspace.sharedWorkspace().runningApplications
         return apps as NSRunningApplication[]!
     }
     
     // NOTE: Crashes on Xcode 6 beta 2
     func windowProcesses() -> AnyObject[]! {
+        #if os(iOS)
+            assert(false, "iOS(UIKit) does not support this feature")
+        #endif
         let list = CGWindowListCopyWindowInfo(CGWindowListOption(kCGWindowListOptionOnScreenOnly), CGWindowID(0))
         let windowInfos = list.takeRetainedValue().__conversion()
         return windowInfos
     }
 }
-
-#endif
