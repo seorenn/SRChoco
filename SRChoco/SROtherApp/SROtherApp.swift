a83abc87de06072b1f7993ebfbe5215a89b8294d
seorenn <hirenn@iplateia.com>
2014-07-07 17:10:09 +0900

    Added module SROtherApp (not tested)

---
+++ SRChoco/SROtherApp/SROtherApp.swift
@@ -0,0 +1,40 @@
+#if os(iOS)
+
+// MARK: SROtherApp for iOS
+import UIKit
+    
+func canLaunchApp(appScheme: String) -> Bool {
+    let appURL = NSURL.URLWithString(appScheme)
+    return UIApplication.sharedApplication().canOpenURL(appURL)
+}
+
+func launchApp(appScheme: String) {
+    let appURL = NSURL.URLWithString(appScheme)
+    UIApplication.sharedApplication().openURL(appURL)
+}
+
+#elseif os(OSX)
+
+// MARK: SROtherApp for OSX
+import Cocoa
+
+func launchApp(appPath: String) {
+    NSWorkspace.sharedWorkspace().launchApplication(appPath)
+}
+
+func activateApp(pid: pid_t) {
+    let app = NSRunningApplication(processIdentifier: pid)
+    activateApp(app)
+}
+    
+func activateApp(bundleIdentifier: String) {
+    let apps = NSRunningApplication.runningApplicationsWithBundleIdentifier(bundleIdentifier)
+    let app: NSRunningApplication! = apps[0] as NSRunningApplication
+    activateApp(app)
+}
+    
+func activateApp(app: NSRunningApplication) {
+    app.activateWithOptions(.ActivateAllWindows)
+}
+    
+#endif
