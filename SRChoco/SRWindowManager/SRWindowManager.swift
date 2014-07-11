1104de47a24f19dcc9fafb33254d606db8465e98
seorenn <hirenn@iplateia.com>
2014-07-11 18:41:53 +0900

    shareing works

--- SRChoco/SRWindowManager/SRWindowManager.swift
+++ SRChoco/SRWindowManager/SRWindowManager.swift
@@ -1,22 +1,28 @@
 #if os(OSX)
 import Cocoa
-#endif
     
 class SRWindowManager {
-    // NOTE: Crashes on Xcode 6 beta 2
+    var capturing = false
+    
+    struct StaticInstance {
+        static var dispatchToken: dispatch_once_t = 0
+        static var instance:SRWindowManager?
+    }
+    
+    class func sharedManager() -> SRWindowManager {
+        dispatch_once(&StaticInstance.dispatchToken) {
+            StaticInstance.instance = SRWindowManager()
+        }
+        return StaticInstance.instance!
+    }
+    
     func processes() -> [NSRunningApplication!] {
-        #if os(iOS)
-            assert(false, "iOS(UIKit) does not support this feature")
-        #endif
         let apps = NSWorkspace.sharedWorkspace().runningApplications
         return apps as [NSRunningApplication!]
     }
 
     func windowProcesses() -> Array<NSRunningApplication?> {
         var apps = Array<NSRunningApplication?>()
-        #if os(iOS)
-            assert(false, "iOS(UIKit) does not support this feature")
-        #endif
         let list = CGWindowListCopyWindowInfo(CGWindowListOption(kCGWindowListExcludeDesktopElements | kCGWindowListOptionOnScreenOnly), CGWindowID(0))
         let windowInfos = list.takeRetainedValue().__conversion() as Array
         for info in windowInfos as Array<Dictionary<String, AnyObject>> {
@@ -30,4 +36,14 @@ class SRWindowManager {
         }
         return apps
     }
+    
+    func startCaptureWindowChanging() {
+        if capturing {
+            return
+        }
+    
+        let notificationCenter = NSWorkspace.sharedWorkspace().notificationCenter;
+        // TODO
+    }
 }
+#endif
