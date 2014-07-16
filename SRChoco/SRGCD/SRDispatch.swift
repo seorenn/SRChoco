4dce64b9df8908b5abcde67647af6aea494f7a5b
seorenn <hirenn@iplateia.com>
2014-07-16 16:57:32 +0900

    New Module - SRGCD

---
+++ SRChoco/SRGCD/SRDispatch.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+class SRDispatch {
+    class func mainTask(job: () -> ()) {
+        dispatch_async(dispatch_get_main_queue()) {
+            job()
+        }
+    }
+    
+    class func backgroundTask(job: () -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+            job()
+        }
+    }
+    
+    class func asyncTask(job: () -> ()) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            job()
+        }
+    }
+}
