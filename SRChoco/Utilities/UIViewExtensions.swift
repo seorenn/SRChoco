67f859864c572b2f7a9564b87742f15103b77bc1
seorenn <hirenn@iplateia.com>
2014-08-18 13:50:57 +0900

    Adding UIView Extensions

---
+++ SRChoco/Utilities/UIViewExtensions.swift
@@ -0,0 +1,82 @@
+#if os(iOS)
+import UIKit
+#endif
+
+#if os(iOS)
+
+extension UIView {
+    var x: CGFloat {
+        set {
+            var frame = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.origin.x
+        }
+    }
+    var y: CGFloat {
+        set {
+            var frame = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.origin.y
+        }
+    }
+    var width: CGFloat {
+        set {
+            var frame = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.size.width
+        }
+    }
+    var height: CGFloat {
+        set {
+            var frame = self.frame
+            frame.size.height = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.size.height
+        }
+    }
+    var origin: CGPoint {
+        set {
+            var frame = self.frame
+            frame.origin = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.origin
+        }
+    }
+    var size: CGSize {
+        set {
+            var frame = self.frame
+            frame.size = newValue
+            self.frame = frame
+        }
+        get {
+            return self.frame.size
+        }
+    }
+    
+    var center: CGPoint {
+        return CGPointMake(self.frame.size.width / 2, self.frame.size.height / 2)
+    }
+    
+    func moveToBottomOfView(view: UIView, margin: CGFloat = 0) {
+        let viewFrame = view.frame
+        var myFrame = self.frame
+        
+        myFrame.origin.y = viewFrame.origin.y + viewFrame.size.height + margin
+        self.frame = myFrame
+    }
+}
+
+#endif
