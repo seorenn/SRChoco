a5a34cb9f1e7e696d4075da7d06a581e74564789
seorenn <hirenn@iplateia.com>
2014-09-25 17:42:30 +0900

    Adding SRRegexpGroups for insteads fucking NSTextCheckingResult

--- SRChoco/SRRegexp/SRRegexp.swift
+++ SRChoco/SRRegexp/SRRegexp.swift
@@ -1,6 +1,38 @@
 import Foundation
 
-struct SRRegexp {
+class SRRegexpGroups: NSObject {
+    let text: String
+    let results: [NSTextCheckingResult]
+    var count: Int {
+        return self.results.count
+    }
+    
+    init(results: [NSTextCheckingResult], text: String) {
+        self.text = text
+        self.results = results
+        super.init()
+        
+        println("SRRegexpGroups initialized with \"\(self.text)\" - \(self.results.count) results")
+    }
+    
+    subscript(index: Int) -> NSTextCheckingResult {
+        return self.results[index]
+    }
+    
+    func string(index: Int) -> String {
+        let range = self.range(index)
+        let result = self.text[range]
+        println("\(self.text) #\(index) = \(result)")
+        return result
+    }
+    
+    func range(index: Int) -> NSRange {
+        let result = self.results[index]
+        return result.range
+    }
+}
+
+class SRRegexp: NSObject {
     private let re: NSRegularExpression?
     
     // MARK: Initializers
@@ -11,16 +43,19 @@ struct SRRegexp {
         if ignoreCase { options = .CaseInsensitive }
         
         self.re = NSRegularExpression(pattern: pattern, options: options, error: &error)
+        super.init()
+
         if self.re == nil { return nil }
     }
     
     // MARK: Functions
     
-    func find(string: String) -> [NSTextCheckingResult]? {
-        let matches = self.re!.matchesInString(string, options: nil, range: NSMakeRange(0, countElements(string)))
+    func find(string: String) -> SRRegexpGroups? {
+        let matches = self.re?.matchesInString(string, options: nil, range: NSMakeRange(0, countElements(string)))
         
-        if matches.count > 0 {
-            return matches as? [NSTextCheckingResult]
+        if matches?.count > 0 {
+            let results = matches! as [NSTextCheckingResult]
+            return SRRegexpGroups(results: results, text: string)
         } else {
             return nil
         }
