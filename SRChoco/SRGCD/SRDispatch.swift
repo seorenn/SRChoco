3fddba2d1080ce0b4cefc57f37afbe48d679ce95
seorenn <hirenn@iplateia.com>
2014-11-12 16:58:15 +0900

    Implementing dispatch_after Helper Methods

--- SRChoco/SRGCD/SRDispatch.swift
+++ SRChoco/SRGCD/SRDispatch.swift
@@ -27,4 +27,27 @@ class SRDispatch {
             job()
         }
     }
+    
+    class func timeForDispatch(delay: Double) -> dispatch_time_t {
+        let d = delay * Double(NSEC_PER_SEC)
+        return dispatch_time(DISPATCH_TIME_NOW, Int64(d))
+    }
+    
+    class func runAfter(delay: Double, job: () -> ()) {
+        dispatch_after(SRDispatch.timeForDispatch(delay), dispatch_get_main_queue()) {
+            job()
+        }
+    }
+    
+    class func backgroundRunAfter(delay: Double, job: () -> ()) {
+        dispatch_after(SRDispatch.timeForDispatch(delay), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
+            job()
+        }
+    }
+    
+    class func asyncRunAfter(delay: Double, job: () -> ()) {
+        dispatch_after(SRDispatch.timeForDispatch(delay), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            job()
+        }
+    }
 }
