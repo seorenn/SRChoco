ba5023de44e14aa11e46447ac0bd502579daa5a0
seorenn <hirenn@iplateia.com>
2014-11-14 15:12:29 +0900

    Removing Unused Playground Codes

--- Demo/SRChocoDemo-OSX/SRChocoDemo-OSX/MyPlayground.playground/section-1.swift
+++ Demo/SRChocoDemo-OSX/SRChocoDemo-OSX/MyPlayground.playground/section-1.swift
@@ -1,10 +1,2 @@
 import Cocoa
 
-let strlist: Array<AnyObject> = ["a", "b", "c"]
-
-if let v = strlist as? [String] {
-    println("strlist is [String]")
-}
-if let v = strlist as? [Int] {
-    println("strlist is [Int]")
-}
