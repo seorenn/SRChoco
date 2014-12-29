5d896e6360585b159043a82b310893925c6c446a
seorenn <hirenn@iplateia.com>
2014-12-29 18:08:49 +0900

    Implements Basic HTTP Features

--- SRChoco/SRHTTP/SRHTTP.swift
+++ SRChoco/SRHTTP/SRHTTP.swift
@@ -10,7 +10,11 @@ import Foundation
 
 typealias SRHTTPResponseBlock = (SRHTTPResponse) -> Void
 
-class SRHTTP: NSObject {
+class SRHTTP: NSObject, NSURLConnectionDataDelegate {
+    
+    private var request: SRHTTPRequest? = nil
+    private var response: SRHTTPResponse? = nil
+    private var responseBlock: SRHTTPResponseBlock? = nil
     
     // MARK: - Public Properties
     
@@ -33,12 +37,25 @@ class SRHTTP: NSObject {
     // MARK: - Simplipied APIs
     
     class func GET(urlString: String, responseBlock: SRHTTPResponseBlock) {
-        // TODO
+        let request = SRHTTPRequest(method: .GET, urlString: urlString)
+        let http = SRHTTP()
+        
+        http.startRequest(request, responseBlock: responseBlock)
     }
     
-    class func POST(urlString: String, formValues: [String: String]?, responseBlock: SRHTTPResponseBlock) {
-        // TODO
+    class func POST(urlString: String, formValues: [String: AnyObject]?, responseBlock: SRHTTPResponseBlock) {
+        let request = SRHTTPRequest(method: .POST, urlString: urlString)
+        if let fv = formValues {
+            request.formValues = fv
+        }
+        let http = SRHTTP()
+        
+        http.startRequest(request, responseBlock: responseBlock)
     }
+    
+    // TODO: class func POST with JSON or File
+    // TODO: class func PUT
+    // TODO: class func DELETE
 
     // MARK: - Public APIs
     
@@ -52,9 +69,44 @@ class SRHTTP: NSObject {
             self.buildCommonHeaders()
         }
     }
+
+    // TODO: Build method arguments spec.
+    func criticalError() {
+        // TODO: Generate NSError
+    }
+    
+    func finish(error: NSError?) {
+        if self.response == nil {
+            self.response = SRHTTPResponse(request: self.request!)
+        }
+        if error != nil {
+            self.response!.error = error
+        }
+        
+        if self.responseBlock != nil {
+            self.responseBlock!(self.response!)
+        }
+        
+        self.responseBlock = nil
+        self.response = nil
+        self.request = nil
+    }
     
     func startRequest(request: SRHTTPRequest, responseBlock: SRHTTPResponseBlock) {
-        // TODO
+        self.request = request
+        
+        let rq = request.urlRequest
+        if rq == nil {
+            self.criticalError(); return    // TODO: criticalError paramters
+        }
+        
+        if let connection = NSURLConnection(request: rq!, delegate: self) {
+            self.responseBlock = responseBlock
+            connection.start()
+        }
+        else {
+            self.criticalError()    // TODO: criticalError parameters
+        }
     }
     
     // MARK: - Private APIs
@@ -71,5 +123,31 @@ class SRHTTP: NSObject {
     
     func buildCommonHeaders() {
         // TODO
+    }
+    
+    // MARK: - NSURLConnectionDataDelegate
+    
+    func connection(connection: NSURLConnection, willCacheResponse cachedResponse: NSCachedURLResponse) -> NSCachedURLResponse? {
+        return cachedResponse
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        self.finish(error)
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        if self.response == nil {
+            self.response = SRHTTPResponse(request: self.request!)
+        }
+        self.response?.responseObject = response
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        if self.response == nil { return }
+        self.response?.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        self.finish(nil)
     }
 }
