922f5d8ac283d12f0d8900cc2334d76b311938c2
Seorenn <hirenn@gmail.com>
2015-05-11 18:32:02 +0900

    New class SRSemaphore that wraps GCD semaphore C functions

---
+++ SRChoco/SRGCD/SRSemaphore.swift
@@ -0,0 +1,30 @@
+//
+//  SRSemaphore.swift
+//  SRChoco
+//
+//  Created by Seorenn on 2015. 5. 11.
+//  Copyright (c) 2015 Seorenn. All rights reserved.
+//
+
+import Foundation
+
+public class SRSemaphore {
+    private let semaphore: dispatch_semaphore_t
+    
+    public init(value: Int = 0) {
+        self.semaphore = dispatch_semaphore_create(value)
+    }
+    
+    public func wait() {
+        dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER)
+    }
+    
+    public func wait(timeout: NSTimeInterval) {
+        let to = Int64(timeout * NSTimeInterval(NSEC_PER_SEC))
+        dispatch_semaphore_wait(self.semaphore, to)
+    }
+    
+    public func signal() {
+        dispatch_semaphore_signal(self.semaphore)
+    }
+}
