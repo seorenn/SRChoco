61a92d0597f7180e8597b9eb132c5182cb94eb35
Seorenn <hirenn@gmail.com>
2015-06-04 17:59:24 +0900

    Start prepare new module SRTextStyler

--- Demo/SRChocoDemo-OSX/SRChocoDemo-OSX/MyPlayground.playground/section-1.swift
+++ Demo/SRChocoDemo-OSX/SRChocoDemo-OSX/MyPlayground.playground/section-1.swift
@@ -1,18 +1,20 @@
 import Cocoa
 
-extension NSColor {
-    convenience init(hex: Int, alpha: CGFloat = 1.0) {
-        let r = CGFloat((hex & 0xff0000) >> 16) / 255.0
-        let g = CGFloat((hex & 0x00ff00) >> 8) / 255.0
-        let b = CGFloat(hex & 0x0000ff) / 255.0
-        self.init(red: r, green: g, blue: b, alpha: alpha)
-    }
-}
+let mstr = NSMutableAttributedString(string: "ABC", attributes: [NSFontAttributeName: NSFont.systemFontOfSize(15),
+    NSForegroundColorAttributeName:NSColor.blackColor()])
 
-let black = NSColor(hex: 0x000000)
-let white = NSColor(hex: 0xffffff)
-let red = NSColor(hex:0xff0000)
-let green = NSColor(hex: 0x00ff00)
-let blue = NSColor(hex: 0x0000ff)
-let opGray = NSColor(hex: 0x000000, alpha: 0.5)
+let elm = NSAttributedString(string: "DEF", attributes: [NSFontAttributeName: NSFont.systemFontOfSize(25),
+    NSForegroundColorAttributeName: NSColor.redColor()])
 
+let elm2 = NSAttributedString(string: "GHI", attributes: [NSFontAttributeName: NSFont.systemFontOfSize(9),
+    NSForegroundColorAttributeName: NSColor.blueColor(),
+    NSBaselineOffsetAttributeName: 8])
+
+mstr.appendAttributedString(elm)
+mstr.appendAttributedString(elm2)
+
+mstr.addAttribute(NSBaselineOffsetAttributeName, value: 4, range: NSMakeRange(0, 3))
+
+
+let field = NSTextField(frame: NSMakeRect(0, 0, 300, 100))
+field.attributedStringValue = mstr
